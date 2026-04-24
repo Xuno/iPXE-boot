788e4cb4ee002ec5741cebf54c2603cc3ca12840
dev <dev@xuno.com>
2026-04-24 06:00:46 +0300

    added app layers independent

--- docker/build.sh
+++ docker/build.sh
@@ -4,8 +4,10 @@ set -euo pipefail
 
 DEST_DIR="http/boots/ubuntu/current"
 BASE_OS_CONTAINER_NAME=base-os
+APPS_CONTAINER_NAME=apps
 
-docker compose --progress=plain  up --build --remove-orphans "$BASE_OS_CONTAINER_NAME"
+docker compose --progress=plain up --build --remove-orphans "$BASE_OS_CONTAINER_NAME"
+docker compose --progress=plain up --build --remove-orphans "$APPS_CONTAINER_NAME"
 docker compose down
 
 echo "Deployment to $DEST_DIR complete."
